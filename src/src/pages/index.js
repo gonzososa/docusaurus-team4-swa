3d4a2cd9a98efb1fa394f4dbf5ae2e8e21fbd9af
Gonzalo Sosa <gonzasosa@outlook.com>
2023-09-30 14:51:11 -0600

    Githublogin enabled

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -1,41 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.modules.css';
 
-import styles from './index.module.css';
+/**
+ * @typedef {object} UserInfo
+ * @prop {"github"} IdentityProvider
+ * @prop {string} userId
+ * @prop {string} userDetails
+ * @prop {string[]} userRoles
+ */
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+/**
+ * @return {UserInfo | null}
+ */
+function useUserInfo () {
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect (() => {
+        async function getUserInfo () {
+            const response = await fetch ('./auth/me');
+            const payload = await response.json ();
+            const { clientPrincipal } = payload;
+
+            return clientPrincipal;
+        }
+    }, []);
+
+    getUserInfo().then((ui) => setUserInfo(ui));
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+export default function Home () {
+    const { siteConfig } = userDocusaurusContext ();
+    const userInfo = useUserInfo ();
+
+    return (
+        <Layout 
+            title={`Hello from ${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
+
+            <header className={clsx('hero hero--primary', styles.heroBanner)}>
+                <div className='container'>
+                    <h1 className='hero__title'>{siteConfig.title}</h1>
+                    <p className='hero--subtitle'>{siteConfig.tagLine}</p>
+                    <div className={styles.buttons}>
+                        {
+                            userInfo ? 
+                                (<p>Hello {userInfo.userDetails}</p>) :
+                                <a 
+                                    className='button button-secondary button-lg'
+                                    href="./auth/login/github"
+                                >
+                                    Click here to login
+                                </a>
+                        }
+                    </div>
+                </div>
+            </header>
+
+        </Layout>
+    );
 }
