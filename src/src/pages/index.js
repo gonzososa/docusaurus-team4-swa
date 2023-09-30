f79bffafc7cfaf99a85dd37d62a92d481a0ea240
Gonzalo Sosa <gonzasosa@outlook.com>
2023-09-30 15:49:47 -0600

    auth tag fixed

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home () {
         <Layout 
             title={`Hello from ${siteConfig.title}`}
             description="Description will go into a meta tag in <head />">
-
+            console.log(siteConfig);
             <header className={clsx('hero hero--primary', styles.heroBanner)}>
                 <div className="container">
                     <h1 className="hero__title">{siteConfig.title}</h1>
